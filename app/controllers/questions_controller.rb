2386426ed925b897c55da618c9d2aeea29941bce
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-02 18:39:58 +0900

    questionsコントローラーのnew, create処理の記述

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,22 @@ class QuestionsController < ApplicationController
   def index
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def question_params
+    params.require(:question).permit(:category_id, :title, :text).merge(user_id: current_user.id)
+  end
 end
