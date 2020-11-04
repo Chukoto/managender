03921f2304a717bb60c66f7805ca4a22f6ba2471
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-04 18:37:01 +0900

    commentsテーブルとアソシエーションの作成

--- db/migrate/20201103064342_create_comments.rb
+++ db/migrate/20201103064342_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.integer :user_id
+      t.integer :question_id
+      t.text :comment_text
       t.timestamps
     end
   end
