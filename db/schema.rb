34631615275c33cc824eed0e88f7025acbcaad85
Haruki Hirai <iamprivateengineer@gmail.com>
2020-11-05 14:41:58 +0900

    migrationとモデルにコメントが空では投稿できない記述

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2020_11_03_073248) do
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "question_id"
-    t.text "comment_text"
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
